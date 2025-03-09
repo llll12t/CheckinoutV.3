5d450a9b719ce5a4b21d38cf9658133f4b6c65c9
llll12t <147978645+llll12t@users.noreply.github.com>
2025-03-09 12:25:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxKtQXKqAOxIOb8Pgh6Gg3h6-YHj8WnPTC0Lhl-Gq_aFSfWXoxfMRSo2vKFY7pa8yP6EQ/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
     const WEB_LEAVE_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
-    const WEB_OT_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
+    const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
 
 //LIFF_ID
     const LIFF_ID = '2006740306-qp2xNvgM'; // LIFF ID Checkin
     const LIFF_ID1 = '2006740306-qp2xNvgM'; // LIFF ID history
     const LIFF_ID2 = '2006740306-qp2xNvgM'; // LIFF ID leave
-    const LIFF_ID4 = '2006740306-pO7Gq31W'; // LIFF ID ot
-    const LIFF_ID3 = '2006740306-qlr7J9go'; // LIFF ID register
+    const LIFF_ID3 = '2006740306-pO7Gq31W'; // LIFF ID ot
+    const LIFF_ID4 = '2006740306-qlr7J9go'; // LIFF ID register
